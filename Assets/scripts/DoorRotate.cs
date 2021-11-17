75be91273a7ee175bc537e8040cd2eef08ee2fa7
kokorirko <gregoire.richard@hetic.net>
2021-11-17 21:38:33 +0100

    door rotation
    Door rotation script in assets > scripts > doorRotate.cs
    La rotation dépendant d'un pivot et la porte doit être l'enfant du pivot pour que ça fonctionne

--- Assets/scripts/DoorRotate.cs
+++ Assets/scripts/DoorRotate.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class DoorRotate : MonoBehaviour
 {
-    public Vector3 Axis = new Vector3(1,0,1);
-    public int rotateSpeed = 10;
+    public Vector3 Axis;
+    public int rotateSpeed = 80;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +15,9 @@ public class DoorRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     /*if(Input.GetButtonDown("Fire1")){
-         transform.Rotate(Axis, rotateSpeed);
-     }   */
+        if(Input.GetButtonDown("Fire1")){
+            transform.Rotate(Axis, rotateSpeed * Time.deltaTime); 
+        }
+        //float inputH = Input.GetAxis("Horizontal");
     }
 }
